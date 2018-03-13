06008413e423c0c186fb04c5171cb64292ef18d7
Blaise <blazej@banaszewski.pl>
2018-03-13 13:43:22 +0100

    perspective transformation

--- perspective_correction.py
+++ perspective_correction.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from IPython.display import clear_output
 
-
-def playvideowin(vidname,winname='video'):
+def playvideowin(vidname, shape, src_points, dst_points, winname='video'):
     vid = cv2.VideoCapture(vidname)
     while(1):
         ret, frame = vid.read()
@@ -14,43 +13,41 @@ def playvideowin(vidname,winname='video'):
             vid.release()
             print("Released Video Resource")
             break
-        
-        #homography = find_homography(frame)
-        #frame = perspective_correction(frame, homography)
+    
+        homography = find_homography(src_points,dst_points)
+        frame = perspective_correction(frame, shape, homography)
 
         cv2.namedWindow(winname)
         cv2.startWindowThread() #this normally isn't required
         cv2.imshow(winname,frame)
-        k=cv2.waitKey(100)
+        k=cv2.waitKey(10)
         
         if k==27: #exit is Esc is pressed
             break
 
     cv2.destroyAllWindows()   
 
-
-def perspective_correction(frame, homography):
+def perspective_correction(frame, shape, homography):
     transformed_frame = cv2.warpPerspective(frame, homography, 
-                                            (frame.shape[1], frame.shape[0]))
+                                            (shape[1], shape[0]))
 
     return transformed_frame
     
-def find_homography(frame, angle = 0):
-    height, width, channels = frame.shape
-    offset = 200
-    dst_points = np.array([[0,0],[width,0],[0+offset,height],
-                           [width-offset,height]])
-                           
-    src_points = np.array([[0,0],[width,0],[0,height],[width,height]])
+def find_homography(src_points,dst_points):
     homography, status = cv2.findHomography(src_points,dst_points)    
 
     return homography
 
 if __name__ == "__main__":
     
-    #playvideowin("../videos/2017_06_22_1439 Krydset Falen Kløvermosevej.mp4")
-    vid = cv2.VideoCapture("../videos/2017_06_22_1439 Krydset Falen Kløvermosevej.mp4")
-    ret, frame = vid.read()    
+    map_reference_BGR = cv2.imread("./images/googlemap.png") 
+    shape = map_reference_BGR.shape
+    src_points = np.array([[74,347],[604,193],[508,60],[309,94]])   
+    dst_points = np.array([[31, 511],[892,451],[963,29],[444,67]])
+    playvideowin("../videos/videoplayback", shape)
+    
+    """
     plt.figure("tokyo")
     plt.imshow(frame)
     plt.show()
+    """
