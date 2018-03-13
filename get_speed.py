71e4d421760b2157f73d49d20d936320191aeae8
Per Breum <lillep3r@hotmail.com>
2018-03-13 19:56:26 +0100

    add get_speed function

---
+++ get_speed.py
@@ -0,0 +1,6 @@
+def calculate_speed(old_pos_x,old_pos_y, new_pos_x, new_pos_y,
+                    grund_sampling_distance):
+    dist_between_coordinates = math.sqrt(pow(new_pos_x-old_pos_x,2) +
+                                         pow(new_pos_y-old_pos_y,2))
+    speed = round(dist_between_coordinates * fps * scale,0)
+    return speed
