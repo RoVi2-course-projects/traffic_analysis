ec55b2b4e564d4cfc9bc8e14d24a0ab7a399ebf0
Per Breum <lillep3r@hotmail.com>
2018-03-13 20:01:27 +0100

    add rounding

--- get_speed.py
+++ get_speed.py
@@ -1,6 +1,8 @@
+# obtain old and new pixels to track
+# ground sampling distance is in cm/pixels
 def calculate_speed(old_pos_x,old_pos_y, new_pos_x, new_pos_y,
                     grund_sampling_distance):
     dist_between_coordinates = math.sqrt(pow(new_pos_x-old_pos_x,2) +
                                          pow(new_pos_y-old_pos_y,2))
-    speed = round(dist_between_coordinates * fps * scale,0)
+    speed = round(dist_between_coordinates * fps * scale,1)
     return speed
