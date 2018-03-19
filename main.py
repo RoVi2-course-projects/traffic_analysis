9240084d2631c3ccd1bc6a5961232772750c3c6a
Per Breum <lillep3r@hotmail.com>
2018-03-19 23:20:04 +0100

    main changed - not working, need to change numpy array

--- main.py
+++ main.py
@@ -1,30 +1,70 @@
 #!/usr/bin/python3
 # Standard libraries
 
-# Download video 
+# Download video
 #https://www.youtube.com/watch?v=0WlKMAga7BY&feature=youtu.be
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-import perspective_correction as pc
-  
+#   import perspective_correction as pc
+import optical_flow_tracking
+from bg_extraction import BackgroundSubtractor
+from optical_flow_tracking import OpticalFlowTracking
+
 
 def main():
     # Paths to files
-    vid_path = "../videos/videoplayback"
+    vid_path = "./resources/trafic-video.mp4"
     map_path = "./resources/googlemap.png"
-    
-    # Load video and shape of the map
-    vid = cv2.VideoCapture(vid_path)
-    shape = pc.get_map_shape(map_path)
+    video = cv2.VideoCapture(vid_path)
+    # Instantiate the main class for subtracting the background.
+    bg_subtractor = BackgroundSubtractor()
+    oft = OpticalFlowTracking()
+    ret, frame = video.read()
 
-    # Perspectiv transformation points, ROI
-    src_points = np.array([[74,347],[604,193],[508,60],[309,94]])   
-    dst_points = np.array([[31,511],[892,451],[963,29],[444,67]])
-    
-    # Play video
-    pc.playvideowin(vid, shape, src_points, dst_points)
+    old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    mask = np.zeros_like(frame)
+
+    while(1):
+        ret, frame = video.read()
+        if not ret:
+            break
+        # Update the video frame and refresh the background image.
+        bg_subtractor.update_frame(frame)
+        fgmask = bg_subtractor.extract_background()
+        centroids = bg_subtractor.find_centroids()
+        centroids = np.asarray(centroids)
+        print(type(centroids))
+        print(centroids)
+
+        centroids, old_gray, mask = oft.optical_flow_tracking(centroids, frame, old_gray, mask)
+
+        # # Draw a circcle surrounding every detected moving shape.
+        # for centroid in centroids:
+        #     cv2.circle(frame, centroid, 10, (255, 0, 0),
+        #                thickness=2)
+        # cv2.imshow('frame', frame)
+        # cv2.imshow('frame2', fgmask)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+    video.release()
+    cv2.destroyAllWindows()
+    return
+
+    # # Load video and shape of the map
+    # vid = cv2.VideoCapture(vid_path)
+    # shape = pc.get_map_shape(map_path)
+    #
+    # # Perspectiv transformation points, ROI
+    # src_points = np.array([[74,347],[604,193],[508,60],[309,94]])
+    # dst_points = np.array([[31,511],[892,451],[963,29],[444,67]])
+    #
+    # # Play video
+    # pc.playvideowin(vid, shape, src_points, dst_points)
+
+
 
 if __name__ == "__main__":
     main()
--- optical_flow_tracking.py
+++ optical_flow_tracking.py
@@ -6,63 +6,64 @@ import get_speed
 import time
 from kalman_filtering_speed import kalman_filter
 
+class OpticalFlowTracking:
+    def draw_optical_flow(self, good_new, good_old, mask, frame):
+        xhat = 0.0 # initial guess, estimate of x
+        p = 1.0 # initial guess, error estimate
+        kf = kalman_filter()
+        for i,(new,old) in enumerate(zip(good_new,good_old)):
+            a,b = new.ravel()
+            c,d = old.ravel()
 
-def draw_optical_flow(good_new, good_old, mask, frame):
-    xhat = 0.0 # initial guess, estimate of x
-    p = 1.0 # initial guess, error estimate
-    kf = kalman_filter()
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
+            speed = get_speed.calculate_speed(a, b, c, d, 50, 25)
+            xhat, p = kf.kalman_filter_speed(speed, xhat, p)
 
-        speed = get_speed.calculate_speed(a, b, c, d, 50, 25)
-        xhat, p = kf.kalman_filter_speed(speed, xhat, p)
+            mask = cv2.line(mask, (a,b),(c,d),(0,0,255), 2)
+            #frame = cv2.circle(frame,(a,b),9,color[i].tolist(),-1)
+            frame = cv2.circle(frame,(a,b),9,(0,0,255),2)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame,str(xhat)[0:4],(a,b), font, 1,(255,255,255),
+                        2,cv2.LINE_AA)
+        return mask, frame
 
-        mask = cv2.line(mask, (a,b),(c,d),(0,0,255), 2)
-        #frame = cv2.circle(frame,(a,b),9,color[i].tolist(),-1)
-        frame = cv2.circle(frame,(a,b),9,(0,0,255),2)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame,str(xhat)[0:4],(a,b), font, 1,(255,255,255),
-                    2,cv2.LINE_AA)
-    return mask, frame
+    # inspiration found from git files referenced here:
+    # https://docs.opencv.org/3.3.1/d7/d8b/tutorial_py_lucas_kanade.html
+    def optical_flow_tracking(self, list_of_poi, frame, old_gray, mask0):
+        # Parameters for lucas kanade optical flow
+        lk_params = dict( winSize  = (15,15),
+                          maxLevel = 2,
+                          criteria = (cv2.TERM_CRITERIA_EPS |
+                                      cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# inspiration found from git files referenced here:
-# https://docs.opencv.org/3.3.1/d7/d8b/tutorial_py_lucas_kanade.html
-def optical_flow_tracking(list_of_poi, frame, old_gray, mask0):
-    # Parameters for lucas kanade optical flow
-    lk_params = dict( winSize  = (15,15),
-                      maxLevel = 2,
-                      criteria = (cv2.TERM_CRITERIA_EPS |
-                                  cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        mask = mask0
+        p0 = list_of_poi
+        old_g = old_gray
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    mask = mask0
-    p0 = list_of_poi
-    old_g = old_gray
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_g, frame_gray, p0,
+                                               None, **lk_params)
+        # Select good points
+        good_new = p1[st==1]
+        good_old = p0[st==1]
 
-    # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_g, frame_gray, p0,
-                                           None, **lk_params)
-    # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
+        # draw the tracks and speed
+        mask, frame = self.draw_optical_flow(good_new, good_old, mask, frame)
+        img = cv2.add(frame,mask)
+        cv2.imshow('frame',img)
 
-    # draw the tracks and speed
-    mask, frame = draw_optical_flow(good_new, good_old, mask, frame)
-    img = cv2.add(frame,mask)
-    cv2.imshow('frame',img)
+        # Now update the previous frame and previous points
+        old_g = frame_gray.copy()
+        p0 = good_new.reshape(-1,1,2)
 
-    # Now update the previous frame and previous points
-    old_g = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
-
-    return p0, old_g, mask
+        return p0, old_g, mask
 
 if __name__ == "__main__":
 
 
     #test
-    cap = cv2.VideoCapture('videoplayback.mp4')
+    oft = OpticalFlowTracking()
+    cap = cv2.VideoCapture('./resources/trafic-video.mp4')
 
     # params for ShiTomasi corner detection
     feature_params = dict( maxCorners = 100,
@@ -77,16 +78,18 @@ if __name__ == "__main__":
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
     p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
-    p0 = p0[38:40,:,:]
+    #p0 = p0[38:40,:,:]
     #p0 = p0[80:82,:,:] # for other cars
     poi = p0
     mask = np.zeros_like(old_frame)
     while(1):
 
         ret,frame = cap.read()
-        poi, old_gray, mask = optical_flow_tracking(poi, frame,
+        poi, old_gray, mask = oft.optical_flow_tracking(poi, frame,
                                                     old_gray, mask)
 
+        print(type(poi))
+        print(poi)
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break
